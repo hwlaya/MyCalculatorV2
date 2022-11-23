a1a2fed02843db1751f116be2e51ca35fd9e4056
hwlaya <main.wayne22@gmail.com>
2022-11-23 10:27:29 +0800

    Initial Commit

---
+++ MyCalculatorV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalculatorV2
+//
+//  Created by Mac User on 11/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
